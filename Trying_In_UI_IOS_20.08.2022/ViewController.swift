0550757e5358d0650f84ccc84d9b559b530d7c06
Sg24n <47612337+Sg24n@users.noreply.github.com>
2022-08-20 17:44:20 +0200

    first commit

---
+++ Trying_In_UI_IOS_20.08.2022/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Trying_In_UI_IOS_20.08.2022
+//
+//  Created by Kira Vaskovskiy on 20.08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
