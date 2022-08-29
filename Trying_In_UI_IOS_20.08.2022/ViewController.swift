dcc8aaaea7284a8145e1e535158b34e9e30d4cf8
Sg24n <47612337+Sg24n@users.noreply.github.com>
2022-08-29 23:01:28 +0200

    last commit. Starting swifUI

--- Trying_In_UI_IOS_20.08.2022/ViewController.swift
+++ Trying_In_UI_IOS_20.08.2022/ViewController.swift
@@ -8,29 +8,54 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var clickCount = 0
+    
     //Объявление кнопочки и её срабатывание
     @IBAction func Butt(_ sender: UIButton) {
         print("Button Works!")
-        Label_1.text = String("Label Works!")
+        //Добавление в лейбл текста
+        insertInLabel()
+        
+        insertTextView()
     }
+    
     @IBOutlet weak var Label_1: UILabel!
+
+   
+    
+    
+    
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
- 
-        
-        
-        
+
     }
 
 
+  
+    //Добавление в лейбл текста
+    func insertInLabel(){
+        clickCount += 1
+        
+        Label_1.text = String("Label Works! \(clickCount), ")
 
-
-
- 
-
+        
+    }
+    
+    @IBOutlet weak var firstTextView: UITextView!
+    
+    func insertTextView() {
+        firstTextView.text = "aaaa"
+    }
+    
+    func getTextView() -> String{
+        firstTextView.
+    }
     
 }
+
 
